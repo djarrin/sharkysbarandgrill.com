14c9d08c2308a0ce3adf0df657385ec9d2bbc936
djarrin <dmjarrin@gmail.com>
2014-09-17 19:41:36 -0400

    basic framework for site is finished (still have to change everything!) need to work with some of the backend stuff to set up pages

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
-<p>pigs are sweet</p>
- </body>
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+    </div> <!-- /container -->
+
+    <?php wp_footer(); ?>
+
+  </body>
 </html>
