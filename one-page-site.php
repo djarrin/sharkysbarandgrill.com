84bd9d558523cb6c828d7fe053aae8671fbfada4
djarrin <dmjarrin@gmail.com>
2015-02-25 20:38:46 -0500

    All basic functionality is present

--- one-page-site.php
+++ one-page-site.php
@@ -11,22 +11,58 @@ Template Name: One Page Theme
     <div class="jumbotron">
       <div class="container">
           
+          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="http://localhost:85/sharkysbarandgrill/wp-content/uploads/2015/02/sharkysSign.png" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <center><img src="http://localhost:85/sharkysbarandgrill/wp-content/uploads/2015/02/sharkysSign.png" alt="..."></center>
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
            
 
             
       </div>
     </div>
- <?php 
-			    $args = array(
-			        'post_type' => 'page',
-			        'order' => 'ASC'
-			    );
-			    $the_query = new WP_Query( $args );		    
-			?>
-			<?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>	
-            
+            <?php 
+                $args = array(
+                    'post_type' => 'page',
+                    'order' => 'ASC'
+                );
+                $the_query = new WP_Query( $args );         
+            ?>
+            <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?> 
+
             <?php get_template_part( 'content', 'page' ); ?>
-			<?php endwhile; endif; ?>
+
+            <?php endwhile; endif; ?>
 <?php get_footer(); ?>
     
 
